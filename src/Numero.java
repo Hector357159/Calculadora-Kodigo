37699bada06cbba73e636eb2f3cce897082794b1
RC22009ues <hec159357@gmail.com>
2024-09-24 22:19:51 -0600

    Primer commit

---
+++ src/Numero.java
@@ -0,0 +1,18 @@
+public class Numero {
+    //Atributos de la clase Numero
+    private int numero;
+
+
+    //Constructor de la clase numero
+    public Numero(int numero) {
+        this.numero = numero;
+    }
+    //Metodo get de el atributo nunero
+    public int getNumero() {
+        return numero;
+    }
+    //Metodo set de el atributo nunero
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
